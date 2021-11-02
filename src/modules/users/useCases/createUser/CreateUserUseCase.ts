bcdb871d8971ca79d666d7a78abf44f8ec37a1b4
Lucas Liuti <dev.lliuti@gmail.com>
2021-11-02 20:33:17 -0300

    Resolving the challenge

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,15 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const alreadyExists = this.usersRepository.findByEmail(email);
+
+    if (alreadyExists) {
+      throw new Error(`User ${name} already exists`);
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
